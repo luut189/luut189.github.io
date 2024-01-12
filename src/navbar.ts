0d8bd22d27290fca34060469ad284e5d367a58ff
luut189 <minhtuongluu74@gmail.com>
2024-01-11 19:13:14 -0500

    feat: used script to create dynamic navbar links

--- src/navbar.ts
+++ src/navbar.ts
@@ -1,15 +1,51 @@
+interface NavBarItem {
+    displayName: string;
+    href: string;
+}
+
+const navbarItems: NavBarItem[] = [
+    {
+        displayName: 'Resume (placeholder)',
+        href: '/',
+    },
+    {
+        displayName: 'Lorem Ipsum',
+        href: '/',
+    },
+    {
+        displayName: 'Lorem Ipsum',
+        href: '/',
+    },
+];
+
+const mainNavbar = document.getElementById('main-navbar') as HTMLElement;
+const sidebar = document.getElementById('sidebar') as HTMLElement;
+navbarItems.forEach((item) => {
+    const childForMain = document.createElement('a');
+    const childForSide = document.createElement('a');
+    childForMain.innerHTML = item.displayName;
+    childForMain.href = item.href;
+    childForMain.className = 'clickable hide-on-mobile';
+
+    childForSide.innerHTML = item.displayName;
+    childForSide.href = item.href;
+    childForSide.className = 'clickable';
+
+    mainNavbar.appendChild(childForMain);
+    mainNavbar.appendChild(document.getElementById('theme-toggle') as HTMLButtonElement);
+    mainNavbar.appendChild(document.getElementById('sidebar-on') as HTMLButtonElement);
+
+    sidebar.appendChild(childForSide);
+});
+
 window.onscroll = () => {
-    const navbar = document.querySelector('#main-navbar') as HTMLElement;
-    const top = navbar.offsetTop;
-    if (window.scrollY > top) {
-        navbar.classList.add('sticky');
-    } else {
-        navbar.classList.remove('sticky');
-    }
+    const navbar = document.getElementById('main-navbar') as HTMLElement;
+    const top = 50;
+    navbar.classList.toggle('sticky', window.scrollY > top);
 };
 
 document.getElementById('sidebar-on')?.addEventListener('click', () => {
-    const sidebar = document.querySelector('.sidebar') as HTMLElement;
+    const sidebar = document.getElementById('sidebar') as HTMLElement;
 
     sidebar.style.display = 'flex';
     sidebar.style.transform = 'translate(100rem)';
@@ -19,7 +55,7 @@ document.getElementById('sidebar-on')?.addEventListener('click', () => {
 });
 
 document.getElementById('sidebar-off')?.addEventListener('click', () => {
-    const sidebar = document.querySelector('.sidebar') as HTMLElement;
+    const sidebar = document.getElementById('sidebar') as HTMLElement;
     sidebar.style.transform = 'translate(0rem)';
     setTimeout(() => {
         sidebar.style.transform = 'translate(100rem)';
