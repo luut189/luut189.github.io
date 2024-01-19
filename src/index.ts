4c628e5fda69a819fdbbfaac54caca3927010bc3
luut189 <minhtuongluu74@gmail.com>
2024-01-19 15:24:19 -0500

    refactor: seperated the code of link section

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,8 @@
 'use strict';
-import { ELEMENT_DELAY } from './constants';
-
-const http = 'https://';
-const linkSaverWrapper = document.getElementById('link-saver') as HTMLDivElement;
-const savedWebsite = [
-    {
-        name: 'Facebook',
-        url: 'facebook.com',
-    },
-    {
-        name: 'YouTube',
-        url: 'youtube.com',
-    },
-];
-
-let linkDelay = ELEMENT_DELAY;
-savedWebsite.forEach((web) => {
-    const link = document.createElement('a');
-    link.className = 'clickable';
-    link.href = http + web.url;
-    link.innerHTML = web.name;
-    link.style.transform = 'scale(0)';
-    setTimeout(() => {
-        link.style.transform = 'scale(1)';
-    }, linkDelay);
-    linkDelay += ELEMENT_DELAY;
-    linkSaverWrapper.appendChild(link);
-});
-
 const anchorClickable = document.getElementsByTagName('a');
 const buttonClickable = document.getElementsByTagName('button');
+console.log(buttonClickable.length);
+
 for (const anchor of anchorClickable) {
     anchor.addEventListener('mouseover', () => {
         anchor.style.transform = 'scale(0.8)';
