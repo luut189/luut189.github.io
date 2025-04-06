d6062a3efc22982f5a7fba82c1b597f727ef5e7d
luut189 <luut189@users.noreply.github.com>
2025-04-06 17:07:21 +0000

    deploy: 6f83c25390202a68ee8c7651ee5386e1e8fb29da

--- src/components/layout/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className='flex items-center justify-center p-4 text-xs text-muted-foreground'>
-            &copy; {new Date().getFullYear()} Tuong Luu. All Rights Reserved.
-        </footer>
-    );
-}
